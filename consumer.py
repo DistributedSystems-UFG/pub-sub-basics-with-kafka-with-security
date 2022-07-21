6a319a4db8fdf305329d1ba0c1dced2436a4a16a
Fábio M. Costa <fmc@ufg.br>
2022-07-20 23:07:31 -0300

    Create consumer.py

---
+++ consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+from const import *
+
+consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+try:
+  topic = sys.argv[1]
+except:
+  print ('Usage: python3 consumer <topic_name>')
+  exit(1)
+  
+consumer.subscribe([topic])
+for msg in consumer:
+    print (msg.value)
