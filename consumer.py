067d6a1c2168531716a2a45896836acbda541b86
Fábio M. Costa <fmc@ufg.br>
2023-06-24 21:05:11 -0300

    Added simple authentication between clients and broker.

--- consumer.py
+++ consumer.py
@@ -2,7 +2,11 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
+                         security_protocol='SASL_PLAINTEXT',
+                         sasl_mechanism='PLAIN',
+                         sasl_plain_username='fmc',
+                         sasl_plain_password='fmc-pass')
 try:
   topic = sys.argv[1]
 except:
--- producer.py
+++ producer.py
@@ -8,7 +8,11 @@ except:
     print ('Usage: python3 producer <topic_name>')
     exit(1)
     
-producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
+                         security_protocol='SASL_PLAINTEXT',
+                         sasl_mechanism='PLAIN',
+                         sasl_plain_username='fmc',
+                         sasl_plain_password='fmc-pass')
 for i in range(100):
     msg = 'My ' + str(i) + 'st message for topic ' + topic
     print ('Sending message: ' + msg)
