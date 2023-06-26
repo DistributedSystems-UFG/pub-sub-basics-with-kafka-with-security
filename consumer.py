0fb83b799dc9b5039c72cc6644905d406a59eff4
Fábio M. Costa <fmc@ufg.br>
2023-06-26 17:36:32 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -3,14 +3,14 @@ from const import *
 import sys
 
 try:
-    topic = sys.argv[1]
-    username = sys.argv[2]
-    password = sys.argv[3]
+  topic = sys.argv[1]
+  username = sys.argv[2]
+  password = sys.argv[3]
 except:
-    print ('Usage: python3 producer <topic_name> <username> <password>')
-    exit(1)
-    
-producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
+  print ('Usage: python3 consumer <topic_name> <username> <password>')
+  exit(1)
+
+consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
                          security_protocol='SASL_PLAINTEXT',
                          sasl_mechanism='PLAIN',
                          sasl_plain_username=username,
