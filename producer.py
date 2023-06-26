2109e42df27cd07f963e52f93c3a7f000191e5a8
Fábio M. Costa <fmc@ufg.br>
2023-06-26 17:35:58 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -4,15 +4,17 @@ import sys
 
 try:
     topic = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
 except:
-    print ('Usage: python3 producer <topic_name>')
+    print ('Usage: python3 producer <topic_name> <username> <password>')
     exit(1)
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
                          security_protocol='SASL_PLAINTEXT',
                          sasl_mechanism='PLAIN',
-                         sasl_plain_username='alice',
-                         sasl_plain_password='alice-pass')
+                         sasl_plain_username=username,
+                         sasl_plain_password=password)
 for i in range(100):
     msg = 'My ' + str(i) + 'st message for topic ' + topic
     print ('Sending message: ' + msg)
